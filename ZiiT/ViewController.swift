bd1b0e43d8e51dcdf27a4acd16500162949da186
Marven Cesar <cesarmarven@gmail.com>
2024-06-03 16:37:18 -0400

    Initial Commit

---
+++ ZiiT/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ZiiT
+//
+//  Created by Marven Cesar on 6/3/24.
+//
+
+import UIKit
+/*
+ - Login Screen
+ - Channel List, Dms Group Chat
+ - message, create chat,channels
+ - settings
+ 
+ */
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       title = "ZiiT"
+        view.backgroundColor = .secondarySystemBackground
+    }
+
+
+}
+
